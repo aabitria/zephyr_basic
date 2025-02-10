6f472bd5d031b3e764b4b46e8312a7e5758805df
Alvin <abitria_alvin@yahoo.com>
2025-02-10 18:54:30 +0800

    Test DT macro for vector properties.

--- src/main.c
+++ src/main.c
@@ -15,6 +15,13 @@
 #define NODE_PROPS_CHOSEN_BY_PATH       DT_CHOSEN(chosen_by_path)
 #define NODE_PROPS_CHOSEN_AS_STRING     DT_CHOSEN(chosen_as_string)
 
+#define NODE_PROPS_BY_LABEL             DT_NODELABEL(label_with_props)
+
+#define PRINTK_STRING(node_id, prop, idx)                                       \
+        do {                                                                    \
+                printk("[%d] -- %s\n", idx, DT_PROP_BY_IDX(node_id, prop, idx));\
+        } while (0);
+
 
 typedef struct {
         bool boolean_exists;
@@ -24,9 +31,11 @@ typedef struct {
         char *enum_string_value;
 } values_t;
 
-// the properties here should be named such as those dashes in .dts is
-// replaced by _ !!
-// Remember, the property name is appended to the token!
+/*
+ * the properties here should be named such as those dashes in .dts is
+ * replaced by _ !!
+ * Remember, the property name is appended to the token in devicetree_gen.h!
+ */
 const values_t values = {
         .boolean_exists    = DT_PROP(NODE_PROPS_ALIAS_BY_LABEL, existent_boolean),
         .int_value         = DT_PROP(NODE_PROPS_ALIAS_BY_PATH, int),
@@ -55,7 +64,24 @@ int main(void)
             "The index within 'enum_string' of the selected value '%s' is %d.\n",
             values.enum_string_value,
             DT_ENUM_IDX(NODE_PROPS_CHOSEN_AS_STRING, enum_string));
+            
+        /*
+         *This executes PRINTK_STRING for each string element of string_array
+         * property.
+         */
+        DT_FOREACH_PROP_ELEM(NODE_PROPS_BY_LABEL, string_array, PRINTK_STRING);
+        
+        const uint32_t cell_array[] = DT_PROP(NODE_PROPS_BY_LABEL, array);
+        const uint8_t bytestring[] = DT_PROP(NODE_PROPS_BY_LABEL, uint8_array);
 
+        const size_t cell_array_len = DT_PROP_LEN(NODE_PROPS_BY_LABEL, array);
+        const size_t bytestring_len = DT_PROP_LEN(NODE_PROPS_BY_LABEL, uint8_array);
+        
+        if ((cell_array_len != (sizeof(cell_array) / sizeof(uint32_t))) ||
+            (bytestring_len != (sizeof(bytestring) / sizeof(uint8_t)))) {
+            /* unreachable since _LEN macro will always match the size */
+            printk("Something's wrong!\n");
+        }
         while (1) {
                 k_msleep(SLEEP_TIME_MS); // Sleep for 100 ms.
         }
